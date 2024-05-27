1353af4e1bd59f1dc8d0d21e90b09e2a2ece7804
Robben <robbenwadlay@163.com>
2024-05-27 14:20:20 +0800

    add template files

---
+++ rollup.config.mjs
@@ -0,0 +1,19 @@
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import { readFileSync } from 'fs';
+import { builtinModules } from 'module';
+const pkg = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), 'utf8'));
+const name = pkg.name.replaceAll(/-|\./g, '_');
+
+const output = ['amd', 'cjs', 'es', 'iife', 'umd', 'system'].map((format) => ({ format, file: `dist/index.${format}.js`, name }));
+
+export default {
+  input: 'src/index.ts',
+  output,
+  external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {}), ...builtinModules],
+  onwarn: (warning) => {
+    throw Object.assign(new Error(), warning);
+  },
+  strictDeprecations: true,
+  plugins: [terser(), typescript()],
+};
