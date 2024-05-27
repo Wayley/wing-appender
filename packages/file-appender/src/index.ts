004edff7fe0f7d43ec268a2f50f48fd75a032aac
Robben <robbenwadlay@163.com>
2024-05-27 16:17:21 +0800

    add sequence for the asynchronous handler

--- packages/file-appender/src/index.ts
+++ packages/file-appender/src/index.ts
@@ -1,10 +1,19 @@
+import Sequence, { type PromiseHandler } from 'wing-sequence';
+import { IAppender } from '../../shared/src/index';
+
 export type ExecuteHandler = (value: string) => Promise<boolean>;
-export class FileAppender {
+
+export class FileAppender implements IAppender {
+  private sequence: Sequence<PromiseHandler<boolean>>;
   private executeHandler: ExecuteHandler;
   constructor(executeHandler: ExecuteHandler) {
+    this.sequence = new Sequence<PromiseHandler<boolean>>();
     this.executeHandler = executeHandler;
   }
-  execute(value: string) {
-    this.executeHandler?.(value);
+  execute(...args: any[]): void {
+    const value = args.join(',');
+    this.sequence.push((flag: boolean) => {
+      return new Promise(async (resolve) => resolve(flag && (await this.executeHandler(value))));
+    });
   }
 }
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,3 @@
 export interface IAppender {
-  execute(value: string): void;
+  execute(...args: any[]): void;
 }
